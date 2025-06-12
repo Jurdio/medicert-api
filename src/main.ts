4efce1fa5793ce9817ca9182e0bbcc659a90b5f4
kupets <kupets@evergreens.in.ua>
2025-06-12 13:07:21 +0300

    feat: integrate swagger docs

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,30 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log('[main.ts] MINT_AUTHORITY_PRIVATE_KEY:', process.env.MINT_AUTHORITY_PRIVATE_KEY);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
-      .setTitle('NFT Cert API')
-      .setDescription('External API to verify NFT medical certificates')
+      .setTitle('Medical Certificates API')
+      .setDescription('API for creating and verifying medical certificates as NFTs on Solana blockchain')
       .setVersion('1.0')
-      .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'API-KEY')
+      .addApiKey({ 
+          type: 'apiKey', 
+          name: 'x-api-key', 
+          in: 'header',
+          description: 'API key for authentication'
+      }, 'api-key')
+      .addTag('certificates', 'Operations with medical certificates')
       .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-  await app.listen(process.env.PORT ?? 3001);
+  SwaggerModule.setup('api-docs', app, document);
+  console.log('ENV:', process.env.EXTERNAL_API_KEY);
+  await app.listen( 3001);
 }
 bootstrap();
