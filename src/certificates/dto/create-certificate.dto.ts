b2d08a9528ba6cecbf71d5c80a1ddb37ba9158a2
kupets <kupets@evergreens.in.ua>
2025-06-12 13:07:08 +0300

    feat: write certificate dto

--- src/certificates/dto/create-certificate.dto.ts
+++ src/certificates/dto/create-certificate.dto.ts
@@ -4,7 +4,8 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateCertificateDto {
     @ApiProperty({
         example: '9c857403d9e7bb1c7a3d6d9c7f2dbf78c3a7c3a7d9c7a3f6b1c7d9e7a3c3a7f6',
-        description: 'SHA256-хеш PDF-довідки',
+        description: 'SHA256 hash of the PDF certificate',
+        required: true
     })
     @IsString()
     @IsNotEmpty()
@@ -12,7 +13,9 @@ export class CreateCertificateDto {
 
     @ApiProperty({
         example: 'fitness',
-        description: 'Тип довідки: vaccination, fitness, illness тощо',
+        description: 'Type of certificate: vaccination, fitness, illness etc.',
+        enum: ['vaccination', 'fitness', 'illness'],
+        required: true
     })
     @IsString()
     @IsNotEmpty()
@@ -20,7 +23,8 @@ export class CreateCertificateDto {
 
     @ApiProperty({
         example: 'Gf4z2NkQydf7Aa2Dpzq98BdZtD8ZT8K3FVbnLdADQ6sE',
-        description: 'Публічний ключ пацієнта (гаманця)',
+        description: 'Solana wallet public key of the patient',
+        required: true
     })
     @IsString()
     @IsNotEmpty()
