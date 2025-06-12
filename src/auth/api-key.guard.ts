96ea17c5f78a47db3be25dedad8664f36b58f92e
kupets <kupets@evergreens.in.ua>
2025-06-12 13:05:10 +0300

    fix: log key guard

--- src/auth/api-key.guard.ts
+++ src/auth/api-key.guard.ts
@@ -1,11 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
+import { EnvironmentConfig } from '../config/configuration';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+    constructor(private configService: ConfigService) {}
+
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest<Request>();
         const apiKey = request.headers['x-api-key'];
-        return apiKey === process.env.EXTERNAL_API_KEY;
+        const expectedApiKey = this.configService.get<EnvironmentConfig>('security.apiKey');
+        
+        console.log('API Key Check:');
+        console.log('Received API Key:', apiKey);
+        console.log('Expected API Key:', expectedApiKey);
+        
+        return apiKey === expectedApiKey;
     }
 }
