5668855b11b93bdc9b3b25981b645713a10052b2
kupets <kupets@evergreens.in.ua>
2025-06-12 13:06:27 +0300

    feat: import env in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,30 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
 import { CertificatesService } from './certificates/certificates.service';
 import { CertificatesController } from './certificates/certificates.controller';
+import { SolanaService } from './blockchain/solana.service';
 import { BlockchainService } from './blockchain/blockchain.service';
 import { PrismaService } from './prisma/prisma.service';
-import {ConfigModule} from "@nestjs/config";
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  }),],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        MINT_AUTHORITY_PRIVATE_KEY: Joi.string().required(),
+        SOLANA_CLUSTER_API: Joi.string().default('https://api.devnet.solana.com'),
+        PORT: Joi.number().default(3000),
+        DEBUG: Joi.boolean().default(false),
+      }),
+    }),
+  ],
   controllers: [CertificatesController],
   providers: [
     CertificatesService,
     BlockchainService,
+    SolanaService,
     PrismaService,
   ],
 })
