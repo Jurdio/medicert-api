c20b05297c86bf9d05a6817ea8fe94064b560f22
kupets <kupets@evergreens.in.ua>
2025-06-12 13:07:02 +0300

    feat: write certificate service

--- src/certificates/certificates.service.ts
+++ src/certificates/certificates.service.ts
@@ -3,6 +3,7 @@ import { CreateCertificateDto } from './dto/create-certificate.dto';
 import { BlockchainService } from '../blockchain/blockchain.service';
 import { PrismaService } from '../prisma/prisma.service'; // імпорт
 
+
 @Injectable()
 export class CertificatesService {
     constructor(
